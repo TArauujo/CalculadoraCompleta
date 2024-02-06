b3e161ff2c07506e2709d367c2b8334518d99935
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-02-05 22:18:17 -0300

    utilizado a função eval para realizar os calculos

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,8 @@
 function adicionarParametro(parametro){
     document.querySelector("[name = 'tela']").value += parametro;
 }
+
+function calcular(){
+    conta = document.querySelector("[name = 'tela']").value;
+    document.querySelector("[name = 'tela']").value = eval(conta)
+}
